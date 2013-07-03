8cfcae874ae99783e1ab69a174a4943cf4861632
Floyd Pink <imbleedingme@googlemail.com>
2013-07-03 17:41:58 -0400

    DELETE repo/:repoId implemented
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- models/repo.js
+++ models/repo.js
@@ -1,19 +1,20 @@
-var debug = require ('debug') ('hub:models:repo'),
-    mongoose = require ('mongoose'),
-    repoSchema = new mongoose.Schema ({
-                                        repoId            : { type : String, unique : true, required : true },
-                                        name              : { type : String, required : true },
-                                        lastBuildFailed   : Boolean,
-                                        lastBuildFinished : Date,
-                                        created           : Date,
-                                        updated           : { type : Date, default : Date.now }
-                                      });
+var debug = require('debug')('hub:models:repo'),
+    mongoose = require('mongoose'),
+    repoSchema = new mongoose.Schema({
+      repoId            : { type : String, unique : true, required : true },
+      name              : { type : String, required : true },
+      devicesSubscribed : { type : Number, default : 1 },
+      lastBuildFailed   : Boolean,
+      lastBuildFinished : Date,
+      created           : Date,
+      updated           : { type : Date, default : Date.now }
+    });
 
 repoSchema.methods.findByRepoId = function (callback) {
-  debug ('Inside models.repo findByRepoId for repoId: %s', this.repoId);
-  return this.model ('Repo').find ({repoId : this.repoId}, callback);
+  debug('Inside models.repo findByRepoId for repoId: %s', this.repoId);
+  return this.model('Repo').find({repoId : this.repoId}, callback);
 };
 
-var Repo = mongoose.model ('Repo', repoSchema);
+var Repo = mongoose.model('Repo', repoSchema);
 
 module.exports = Repo;
--- routes/devices/repos/index.js
+++ routes/devices/repos/index.js
@@ -35,9 +35,9 @@ module.exports = function (app) {
 
       ensureDevice(res, result, requestBody.deviceId, function (device) {
 
-        if (!~device.repos.indexOf(requestBody.repo.repoId)) {
+        if (!~device.repos.indexOf(requestBody.repo.repoId)) {  // device.repos does not have this repoId
 
-          //push repoId to device.repos if does not exist and send response
+          //push repoId to device.repos
           device.repos.push(requestBody.repo.repoId);
           device.updated = Date.now();
           device.save(function (err, device) {
@@ -46,10 +46,11 @@ module.exports = function (app) {
             } else {
 
               //insert Repo document if not exists - fire and forget
+              debug('About to create or update the repo %s', requestBody.repo.repoId);
               new Repo({ repoId : requestBody.repo.repoId }).findByRepoId(function (err, repos) {
                 if (err) {
                   sendErrorResponse(res, result, err);
-                } else if (!repos.length) {
+                } else if (!repos.length) { // we haven't seen this repo before
 
                   var newRepo = extend(new Repo({ repoId : requestBody.repo.repoId, created : Date.now() }), requestBody.repo);
                   newRepo.save(function (err, repo) {
@@ -58,21 +59,29 @@ module.exports = function (app) {
                     }
                   }); //end newRepo save
 
-                } else {
-                  debug('Repo exists via some other device', repos[0]);
-                  Repo.update(repos[0], { updated : Date.now() });
+                } else {  // some other device has subscribed this repo before
+
+                  debug('Repo exists via some other device');
+                  Repo.findOneAndUpdate(repos[0], { updated : Date.now(), devicesSubscribed : repos[0].devicesSubscribed + 1 }, function (err, repo) {
+                    if (err) {
+                      debug('Error updating existing repo: ', err)
+                    } else {
+                      debug('Existing repo updated: ', repo)
+                    }
+                  });
+
                 }
               }); //end newRepo find
 
               debug('Device %s subscribed to the repo %s', requestBody.deviceId, requestBody.repo.repoId);
-              result.status = 'Repo Added';
+              result.status = 'Repo subscribed';
               result.device = device;
               res.send(201, result);
 
             }
           }); //end device save
 
-        } else {
+        } else { // device.repos has this repoId
           debug('Device %s already subscribes to the repo %s', requestBody.deviceId, requestBody.repo.repoId);
           result.status = 'Device already subscribes to this repo.';
           result.device = device;
@@ -80,6 +89,7 @@ module.exports = function (app) {
         }
 
       }); //end ensureDevice
+
     } else {
       sendErrorResponse(res, result, helpers.INVALID_REQUEST_ERROR, 400);
     }
@@ -93,10 +103,69 @@ module.exports = function (app) {
 
     if (helpers.validateDeviceRepo(req, requestBody)) {
 
-      //TODO: remove repo from repos collection for this device
+      ensureDevice(res, result, requestBody.deviceId, function (device) {
+
+        if (!~device.repos.indexOf(requestBody.repo.repoId)) {  // device.repos does not have this repoId
+
+          debug('Device %s does not subscribe to the repo %s', requestBody.deviceId, requestBody.repo.repoId);
+          result.status = 'Device does not subscribe to this repo.';
+          result.device = device;
+          res.send(result);
+
+        } else { // device.repos has this repoId
+
+          //remove repoId from device.repos
+          device.repos.splice(device.repos.indexOf(requestBody.repo.repoId), 1);
+          device.updated = Date.now();
+          device.save(function (err, device) {
+            if (err) {
+              sendErrorResponse(res, result, err);
+            } else {
+
+              //delete Repo document if this device was the last one subscribed to it - fire and forget
+              debug('About to remove or update the repo %s', requestBody.repo.repoId);
+              new Repo({ repoId : requestBody.repo.repoId }).findByRepoId(function (err, repos) {
+                if (err) {
+                  sendErrorResponse(res, result, err);
+                } else if (!repos.length) { // for some reason the repo is missing ! Should not happen...
+                  debug('Repo document is missing for repoId: %s', requestBody.repo.repoId);
+                } else {  // if devicesSubscribed is 1, delete this repo, otherwise devicesSubscribed-- and update
+
+                  var repo = repos[0];
+                  if (repo.devicesSubscribed === 1) { // this was the only device subscribed to this repo
+                    Repo.remove(repo, function (err) {
+                      if (!err) {
+                        debug('Repo removed.');
+                      }
+                    });
+                  } else { // there are other devices still subscribed to this repo
+                    debug('Repo still subscribed by some other device(s)');
+                    Repo.findOneAndUpdate(repos[0], { updated : Date.now(), devicesSubscribed : repos[0].devicesSubscribed - 1 }, function (err, repo) {
+                      if (err) {
+                        debug('Error updating existing repo: ', err)
+                      } else {
+                        debug('Existing repo updated: ', repo)
+                      }
+                    });
+                  }
+
+                }
+              }); //end newRepo find
+
+              debug('Device %s unsubscribed from the repo %s', requestBody.deviceId, requestBody.repo.repoId);
+              result.status = 'Repo unsubscribed';
+              result.device = device;
+              res.send(result);
+
+            }
+          }); //end device save
+
+        }
+
+      }); //end ensureDevice
 
     } else {
-      sendErrorResponse(res, result);
+      sendErrorResponse(res, result, helpers.INVALID_REQUEST_ERROR, 400);
     }
 
   });
@@ -111,7 +180,7 @@ module.exports = function (app) {
       //TODO: remove all repos from repos collection for this device
 
     } else {
-      sendErrorResponse(res, result);
+      sendErrorResponse(res, result, helpers.INVALID_REQUEST_ERROR, 400);
     }
 
   });
